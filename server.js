a390b0537065b7c58665cb478cdf0a4cc5cf6f06
Jose <juegosaplicaciones61@gmail.com>
2026-05-22 09:25:33 -0500

    endpoint expenses

--- server.js
+++ server.js
@@ -6,7 +6,31 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.post("/income", (req, res) => {
+app.post("/expenses", (req, res) => {
 
+  const { type, description, value, date } = req.body;
+
+  const sql = `
+    INSERT INTO expenses (type, description, value, date)
+    VALUES (?, ?, ?, ?)
+  `;
+
+  db.query(
+    sql,
+    [type, description, value, date],
+    (err, result) => {
+
+      if (err) {
+        console.log(err);
+        res.status(500).send("Error guardando gasto");
+      } else {
+        res.send("Gasto guardado correctamente");
+      }
+
+    }
+  );
+
+});
   const { client, route, value, date } = req.body;
 
   const sql = `
