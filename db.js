f32de5a30c33dbd78780fbe0351f7b7d3b2f6685
Jose <juegosaplicaciones61@gmail.com>
2026-05-22 11:19:39 -0500

    use database url

--- db.js
+++ db.js
@@ -1,21 +1,14 @@
 const mysql = require("mysql2");
-console.log(process.env.MYSQLHOST);
-console.log(process.env.MYSQLUSER);
-console.log(process.env.MYSQLDATABASE);
-console.log(process.env.MYSQLPORT);
 
-const connection = mysql.createConnection({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT
-});
+const connection = mysql.createConnection(
+  process.env.DATABASE_URL
+);
 
 connection.connect((err) => {
 
   if (err) {
-    console.log("Error conexión:", err);
+    console.log("ERROR MYSQL:");
+    console.log(err);
   } else {
     console.log("MySQL conectado");
   }
